1081bef7bac4fcc4fb018bfa2b39594dc2f442ba
JoseDanielR15 <josedanielr163@gmail.com>
2026-03-19 11:23:37 -0600

    Semana 10
    Actualizacion de la Base de Datos
    
    Listado de productos
    
    Consultar y Actualizar Perfil de Usuario
    
    Entre otras modificaciones y agregaciones

---
+++ Controllers/ProductoController.php
@@ -0,0 +1,26 @@
+<?php
+include_once $_SERVER["DOCUMENT_ROOT"] . "/MN_JDR/Controllers/UtilitarioController.php";
+include_once $_SERVER["DOCUMENT_ROOT"] . "/MN_JDR/Models/ProductoModel.php";
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+function ConsultarProductos()
+{
+    return ConsultarProductosModel();
+}
+
+if (isset($_POST["btnCambiarEstado"])) {
+
+    $consecutivoProducto = $_POST["Consecutivo"];
+
+    // $result = RegistrarModel($identificacion, $nombre, $contrasenna, $correoElectronico);
+
+    // if ($result) {
+    //     header("Location: ../../Views/vHome/login.php");
+    //     exit;
+    // } else {
+    //     $_POST["Mensaje"] = "Su información no fue registrada correctamente";
+    // }
+}
---
+++ Views/vProducto/consultarProductos.php
@@ -0,0 +1,116 @@
+<?php
+include_once $_SERVER["DOCUMENT_ROOT"] . "/MN_JDR/Views/layout.php";
+include_once $_SERVER["DOCUMENT_ROOT"] . "/MN_JDR/Controllers/ProductoController.php";
+
+$datosProductos = ConsultarProductos();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+MostrarCSS();
+?>
+
+<body>
+
+  <div id="preloader">
+    <div class="spinner"></div>
+  </div>
+
+  <?php
+  MostrarNav();
+  ?>
+
+  <div class="overlay"></div>
+  <main class="main-wrapper">
+
+    <?php
+    MostrarHeader();
+    ?>
+
+    <section class="section">
+            <div class="container-fluid">
+                <div class="title-wrapper pt-30">
+                    <div class="row align-items-center">
+                        <div class="col-md-11 mx-auto">
+                            <div class="card-style mt-5">
+
+                                <?php
+                                if (isset($_POST["Mensaje"])) {
+                                    echo
+                                    '<div class="alert alert-danger text-center" role="alert">
+                                                            ' . $_POST["Mensaje"] . '
+                                                        </div>';
+                                }
+                                ?>
+
+                                <h3 class="mb-15">Productos</h3>
+
+                                
+                                    <div class="row">
+                                        
+                                        <table class="table table-responsive">
+                                          <thead>
+                                            <tr>
+                                              <th>#</th>
+                                              <th>Nombre</th>
+                                              <th>Precio</th>
+                                              <th>Cantidad</th>
+                                              <th>Estado</th>
+                                              <th>Imagen</th>
+                                              <th>Acciones</th>
+                                            </tr>
+                                          </thead>
+                                          <tbody>
+                                          
+                                            <?php
+                                            foreach ($datosProductos as $producto) {
+                                                echo
+                                                '<tr>
+                                                  <td>' . $producto["Consecutivo"] . '</td>
+                                                  <td>' . $producto["Nombre"] . '</td>
+                                                  <td>' . $producto["Precio"] . '</td>
+                                                  <td>' . $producto["Cantidad"] . '</td>
+                                                  <td>' . $producto["EstadoDescripcion"] . '</td>
+                                                  <td><img src="' . $producto["Imagen"] . '" alt="Imagen del producto" width="100"></td>
+                                                  <td>
+                                                    <form action="" method="POST">
+
+                                                      <input type="hidden" name="Consecutivo" value="' . $producto["Consecutivo"] . '">
+
+                                                      <button id="btnCambiarEstado" name="btnCambiarEstado" type="submit" class="btn btn-sm btn-info" title="Cambiar Estado">
+                                                        <i class="fa-solid fa-rotate"></i>
+                                                      </button>
+                                                    </form>
+                                                  </td>
+                                                </tr>';
+                                            }
+                                            ?>
+
+                                          </body>
+                                        </table>
+                                        
+                                    </div>
+                               
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    <?php
+    MostrarFooter()
+    ?>
+
+  </main>
+
+  <?php
+  MostrarJS();
+  ?>
+
+</body>
+
+</html>
